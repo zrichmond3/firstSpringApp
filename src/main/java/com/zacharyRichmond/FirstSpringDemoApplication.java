8a77b3f6f026d199f33b9fb48f9b805b6262980c
Richmond <ZACHARY_RICHMOND@homedepot.com>
2017-03-01 17:14:46 -0500

    first commit

---
+++ src/main/java/com/zacharyRichmond/FirstSpringDemoApplication.java
@@ -0,0 +1,12 @@
+package com.zacharyRichmond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zacharyRichmond/FirstSpringDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zacharyRichmond;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirstSpringDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
